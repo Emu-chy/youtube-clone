d41c77ba6dfa13a8ee4797a9a48125946b8c041d
Emon Chowdhury <emonchowdhury942858@gmail.com>
2023-03-16 17:01:11 +0600

    Add New Video Design

--- src/components/Video.component.jsx
+++ src/components/Video.component.jsx
@@ -2,20 +2,35 @@ import React from "react";
 
 const Video = ({ imgUrl, title, description, videoId, channelTitle }) => {
     return (
-        <div className={`list-group-item d-flex`}>
-            <div className="row">
-                <div className="col-lg-6 mr-auto">
-                    <img src={imgUrl} className="image-list" alt="" />
-                </div>
-                <div className="col-lg-6 ps-0">
-                    <div>
-                        <span className="heading-wrap">
-                            <p className="list-heading">{title}</p>
-                        </span>
-                        <p className="list-description">{channelTitle}</p>
-                        <p className="list-description">{description}</p>
-                    </div>
-                </div>
+        // <div className={`list-group-item mb-3`}>
+        //     <div>
+        //         <div className="col-lg-8 mr-auto">
+        //             <img
+        //                 src={imgUrl}
+        //                 className="list-image"
+        //                 style={{ height: "300px", width: "320px" }}
+        //                 alt=""
+        //             />
+        //         </div>
+        //         <div className="col-lg-4 ps-0">
+        //             <div>
+        //                 <span className="heading-wrap">
+        //                     <p className="list-heading">{title}</p>
+        //                 </span>
+        //                 <p className="list-description">{channelTitle}</p>
+        //                 <p className="list-description"> {description} </p>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+        <div class="card mb-3">
+            <img src={imgUrl} className="card-img-top" alt="" />
+            <div class="card-body">
+                <h5 class="card-title">{title}</h5>
+                <p class="card-text">{channelTitle}</p>
+                <p class="card-text">
+                    <small class="text-muted">{description}</small>
+                </p>
             </div>
         </div>
     );
